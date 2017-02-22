78d2792ab7c0f741e9d956368b5a3dd2f557804e
Stefan <stefandeyoung@gmail.com>
2017-02-21 21:31:01 -0500

    Switched to vectors. Moved ui to separate js

--- sketch.js
+++ sketch.js
@@ -61,25 +61,6 @@ function draw() {
     }
 }
 
-function drawScoreBoard(){
-    textSize(20);
-    fill(0);
-    text("score", 10, 30);
-    text(score, 10+20, 50);
-    text("time",10+60, 30);
-    text(roundLength - round(time),10+73, 50);
-}
-
-function drawGameOver(){
-    background(50);
-    fill(255);
-    textSize(32);
-    text("GAME OVER!", width/2 - 100, height/2);
-
-    textSize(20);
-    text("SCORE: " + score, width/2 + 60 - 100, height/2 + 30);
-}
-
 function replenishBubbles(){
     //Add 3 bubbles every tenth of a round
     var addFreq = roundLength/10*fps;
